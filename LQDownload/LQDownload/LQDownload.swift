f0ae854d18d2e4f00f465e752c2b5c70c10cee71
Alvin-Yang <ylq7777777@gmail.com>
2017-01-08 00:36:13 +0800

    feat: create file store

--- LQDownload/LQDownload/LQDownload.swift
+++ LQDownload/LQDownload/LQDownload.swift
@@ -9,14 +9,22 @@
 import UIKit
 
 class LQDownload: NSObject {
+	
 	//下载路径: 重新下载或者重新生成的数据放在Library/Caches里面
 	func downloadDirectory() -> String {
 		return NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).last!.appending("/TTCache")
 	}
-	//文件全路径
-	func fileFullPath(_ fileName: String) -> String {
-		return downloadDirectory() + "\(fileName)"
+	
+	//文件名
+	func fileName(_ url: String) -> String? {
+		return url.components(separatedBy: "/").last
 	}
+	
+	//文件全路径
+	func fileFullPath(_ url: String) -> String {
+		return downloadDirectory() + "\(fileFullPath(url))"
+	}
+	
 	//文件已下载长度
 	func fileDownloadLength(_ url: String) -> UInt64? {
 		var filesize: UInt64?
@@ -29,4 +37,29 @@ class LQDownload: NSObject {
 		return filesize
 	}
 	
+	//存储文件总长度的文件路径
+	func totalLengthFullPath() -> String{
+		return downloadDirectory().appending("/totalLength.plist")
+	}
+	
+	lazy var tasks = { return [String: String]() }()
+	lazy var sessionModels = { return [String: String]() }()
+	
+	//单例
+	private static let instance = LQDownload()
+	class func shared() -> LQDownload {
+		return instance
+	}
+	
+	//创建下载目录文件
+	func createDownloadDirectory() {
+		if !FileManager.default.fileExists(atPath: downloadDirectory()) {
+			do{ try FileManager.default.createDirectory(atPath: downloadDirectory(), withIntermediateDirectories: true, attributes: nil)
+			} catch {
+				debugPrint("Error: \(error)")
+			}
+		}
+	}
+	
+	//开启任务, 下载资源
 }
