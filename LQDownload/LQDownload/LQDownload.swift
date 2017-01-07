90e015454bc6eec070e7053177b4e318cb68ed17
Alvin-Yang <ylq7777777@gmail.com>
2017-01-07 11:07:17 +0800

    feat: get Download length

--- LQDownload/LQDownload/LQDownload.swift
+++ LQDownload/LQDownload/LQDownload.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class LQDownload: NSObject {
+	//下载路径: 重新下载或者重新生成的数据放在Library/Caches里面
 	func downloadDirectory() -> String {
-		return NSHomeDirectory() + "/TTDownload"
+		return NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).last!.appending("/TTCache")
 	}
-	
+	//文件全路径
 	func fileFullPath(_ fileName: String) -> String {
 		return downloadDirectory() + "\(fileName)"
 	}
-	
+	//文件已下载长度
+	func fileDownloadLength(_ url: String) -> UInt64? {
+		var filesize: UInt64?
+		do {
+			let attr = try FileManager.default.attributesOfItem(atPath: fileFullPath(url))
+			filesize = attr[FileAttributeKey.size] as? UInt64
+		} catch {
+			debugPrint("Error: \(error)")
+		}
+		return filesize
+	}
 	
 }
