a33fa595f2e5540939ce93747bfb76fd32673da3
Alvin-Yang <ylq7777777@gmail.com>
2017-04-17 22:07:54 +0800

    Revert "feat: some tip"
    This reverts commit 9d55ac5ead2d892a396e9d51140a3076dc503afd.

--- SwiftTips/SwiftTips/ViewController.swift
+++ SwiftTips/SwiftTips/ViewController.swift
@@ -55,98 +55,16 @@ class ViewController: UIViewController {
     synchronized(a) {
       //a 不会被其他线程改变
     }
-		
-		testHelloSelf()
-		
-		var mutableArray = [1,2,3]
-		for _ in mutableArray {
-			mutableArray.removeLast()
-			printLog(mutableArray)
-		}
-		printLog(mutableArray)
-		
-		let array = [1, 2, 3, 4, 5, 6]
-		let arrays = array[1...5]
-		
-		print(arrays[1])
-		
-		
-		
-		
-		struct Person {
-			
-			var store = "dasd"
-			
-			var name: String {
-				 get {
-					return store
-				}
-			}
-		}
-		
-		var p = Person()
-		p.store = "ooooooo"
-		
-		print(p.name)
-		p.store = "KKKK"
-		
-		print(p.name)
-		
-		
-		let length1 = "string".characters.count
-		let length2 = "string".data(using: .utf8)
-		let length3 = ("string" as NSString).length
+    
+    
   }
-	
-	
-	
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
   
-	
-	
+  
   
 }
 
-class Book {
-	var array = [Book]()
-	
-	typealias Element = Book
-	
-	init(arrayLiteral elements: [Element]) {
-		array = elements
-	}
-}
-
-class BookListGenerator : IteratorProtocol {
-	typealias Element = Book
-	var currentIndex:Int = 3
-	var bookList:[Book]?
-	init(bookList: [Book]) {
-		self.bookList = bookList
-	}
-	func next() -> Element? {
-		guard let list = bookList else { return  nil }
-		if currentIndex < list.count {
-			let element = list[currentIndex]
-			currentIndex += 1
-			return element
-		}else {
-			return nil
-		}
-	}
-}
-
-
-
-struct Affine {
-	var a: Int
-	var b: Int
-}
-
-func ~=(lhs: Affine, rhs: Int) -> Bool {
-	
-	return lhs.a > rhs
-}
